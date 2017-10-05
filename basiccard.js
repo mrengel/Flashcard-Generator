db0b1b70a25469de48e4f7f1007eb6904a67abc0
mrengel <m.r.engel2010@gmail.com>
2017-10-04 22:29:38 -0400

    Cleaned code

--- basiccard.js
+++ basiccard.js
@@ -1,24 +1,8 @@
-var inquirer = require("inquirer");
 
 var BasicCard = function(front, back){
 	this.front= front;
 	this.back = back;
-	this.trivia = function(){
-		inquirer.prompt([
-		{
-			name: "front",
-			message: this.front
-
-		},
-			]).then(function(answers){
-				if(answers.name===this.back){
-					console.log(answers.name + "\n---------");
-				}
-
-			});
-
-	}
-}
+};
 
 
 
--- clozecard.js
+++ clozecard.js
@@ -1,14 +1,7 @@
 var ClozeCard = function(text, cloze){
 	this.fullText = text;
 	this.cloze = cloze;
-	this.partial = text.replace(cloze, "....");
-	// this.clozeOccur = function() {
-	// 	var occur = text.indexOf(cloze);
-	//  	if(occur===-1){
-	//  		console.log("Cloze does not appear in text");
-	//  	}
-	//  	console.log("'" + this.partial + "'");
-	//  };
+	this.partial = text.replace(cloze, "....");	
 };
 
 ClozeCard.prototype.displayPartial = function(){
@@ -20,17 +13,6 @@ ClozeCard.prototype.displayPartial = function(){
  };
 
 
-var questionOne = new ClozeCard(
-	"Indians is the name of the MLB team in Cleveland.", "Indians");
-var questionThree = new ClozeCard(
- "Thome holds the Indians record for most homeruns.", "edwin");
 
-//console.log(questionOne.partial);
-
-//questionOne.clozeOccur();
-
-questionOne.displayPartial();
-
-questionThree.displayPartial();
 
 module.exports = ClozeCard;
