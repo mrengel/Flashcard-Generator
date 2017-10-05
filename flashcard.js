433c5326a5f97ed9b6c28dc2d0e4d409b217bff6
mrengel <m.r.engel2010@gmail.com>
2017-10-04 22:32:18 -0400

    Do not need

--- flashcard.js
+++ flashcard.js
@@ -1,20 +1,23 @@
-var fs = require("fs");
-var BasicCard = require("./basiccard.js");
-var ClozeCard = require("./clozecard.js");
+//Do not need for assignment
 
-var questionOne = new BasicCard(
-	"What is the name of the MLB team in Cleveland", "Indians");
 
-var questionTwo = new BasicCard(
-	"Which pitcher holds the Indians record for most wins", "Feller");
+// var fs = require("fs");
+// var BasicCard = require("./basiccard.js");
+// var ClozeCard = require("./clozecard.js");
 
-var questionThree = new BasicCard(
-	"Which hitter holds the Indians record for most homeruns", "Thome");
+// var questionOne = new BasicCard(
+// 	"What is the name of the MLB team in Cleveland", "Indians");
 
-var questionFour = new BasicCard(
-	"The Indians have won how many World Series Championships?", "Two");
+// var questionTwo = new BasicCard(
+// 	"Which pitcher holds the Indians record for most wins", "Feller");
 
-var questionFive = new BasicCard(
-	"The Indians are members of which league?", "American");
+// var questionThree = new BasicCard(
+// 	"Which hitter holds the Indians record for most homeruns", "Thome");
 
-questionOne.trivia();
+// var questionFour = new BasicCard(
+// 	"The Indians have won how many World Series Championships?", "Two");
+
+// var questionFive = new BasicCard(
+// 	"The Indians are members of which league?", "American");
+
+// questionOne.trivia();
